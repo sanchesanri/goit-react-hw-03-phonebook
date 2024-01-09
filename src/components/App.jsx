c23e27559bf4a4e5047092cc4fc657b6f97375b9
AnriSanches <89216270+sanchesanri@users.noreply.github.com>
2024-01-09 15:02:22 +0200

    sdad

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,67 +1,77 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
 
-import { FormContact } from "./FormContact/FormContact";
-import { ContactsList } from "./ContactsList/ContactsList";
-import { ContactsFilter } from "./ContactsFilter/ContactsFilter";
+import { FormContact } from './FormContact/FormContact';
+import { ContactsList } from './ContactsList/ContactsList';
+import { ContactsFilter } from './ContactsFilter/ContactsFilter';
 
-const CONTACTSLOCALST = 'contacts'
+const CONTACTSLOCALST = 'contacts';
 
 export class App extends Component {
-
   state = {
     contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Her Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },],
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
-  }
-
+  };
 
   componentDidMount() {
     if (!localStorage.getItem(CONTACTSLOCALST)) {
-      return
+      return;
     }
 
-    const localData = localStorage.getItem(CONTACTSLOCALST)
-    this.setState({ contacts: JSON.parse(localData) })
+    const localData = localStorage.getItem(CONTACTSLOCALST);
+    this.setState({ contacts: JSON.parse(localData) });
   }
 
   componentDidUpdate(prevProps, prevState) {
-    localStorage.setItem(CONTACTSLOCALST, JSON.stringify(this.state.contacts))
+    if (prevState.contact !== this.state.contacts) {
+      localStorage.setItem(
+        CONTACTSLOCALST,
+        JSON.stringify(this.state.contacts)
+      );
+    }
   }
 
-  handleAddContact = (obj) => {
+  handleAddContact = obj => {
     if (this.isInContacts(obj)) {
-      return alert(`${obj.name} is already in contacts`)
+      return alert(`${obj.name} is already in contacts`);
     }
 
     this.setState(prevState => ({
-      contacts: [...prevState.contacts, obj]
-    })
-    )
-    localStorage.setItem(CONTACTSLOCALST, JSON.stringify(this.state.contacts))
-  }
+      contacts: [...prevState.contacts, obj],
+    }));
+    localStorage.setItem(CONTACTSLOCALST, JSON.stringify(this.state.contacts));
+  };
 
-  handlerFilterChange = (event) => {
-    this.setState({ filter: event.target.value })
-  }
+  handlerFilterChange = event => {
+    this.setState({ filter: event.target.value });
+  };
 
   isInContacts = ({ name }) => {
     const { contacts } = this.state;
 
-    return contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())
-  }
+    return contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+  };
 
-  handleDeleteContact = (contactId) => {
+  handleDeleteContact = contactId => {
     this.setState(prevState => {
-      return ({ contacts: prevState.contacts.filter(contact => contact.id !== contactId) })
-    })
-  }
-
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== contactId
+        ),
+      };
+    });
+  };
 
   render() {
-    const filteredContacts = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(this.state.filter.toLowerCase()));
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
 
     return (
       <div>
@@ -70,14 +80,15 @@ export class App extends Component {
         <h2>Contacts</h2>
         <ContactsFilter
           dataContacts={this.state}
-          handlerFilterChange={this.handlerFilterChange} />
-
-        {this.state.contacts.length > 0 &&
+          handlerFilterChange={this.handlerFilterChange}
+        />
+        {this.state.contacts.length > 0 && (
           <ContactsList
             options={filteredContacts}
             handleDeleteContact={this.handleDeleteContact}
-          />}
+          />
+        )}
       </div>
-    )
+    );
   }
-};
+}
